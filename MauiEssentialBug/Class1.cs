ee84efe4f17a8b1fc8e1cf4ea260a9dab0ea6da6
Ewan <git@ewan.emccartney.co.uk>
2023-11-15 10:48:47 +0000

    Init

---
+++ MauiEssentialBug/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Maui.Networking;
+
+namespace MauiEssentialBug
+{
+    public class Class1
+    {
+        public void Foo()
+        {
+            var x = Connectivity.NetworkAccess;
+            System.Diagnostics.Debug.WriteLine($"Foo = {x}");
+        }
+    }
+}
